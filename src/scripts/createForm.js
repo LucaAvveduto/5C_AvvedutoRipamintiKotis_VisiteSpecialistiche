7c3d1152192fc0977c5ae8e24fc695b9a039a42b
Luca Avveduto <l.avveduto00@gmail.com>
2024-10-29 14:14:10 +0100

    form works

--- src/scripts/createForm.js
+++ src/scripts/createForm.js
@@ -1,25 +1,23 @@
 export function createForm(parentElement) {
-    let data = {};
+    let data = [];
     let callback = null;
     return {
         setLabels: (labels) => { data = labels;},  
         onsubmit: (callbackInput) => { callback = callbackInput},
         render: () => { 
-            let html = "<div class='txt-field'>Data<input type='date' class='form-control' id='dateInput'></div>\n";
-            for(const key in data)  html += "<div class='txt-field'>" + key + "<input type='text' class='form-control' id='" + key + "'></div>\n";    
+            let html = data.map((name) => {
+                return "<div>" + name + "\n<input type='text' class='form-control' id='" + name + "'/></div>";
+            }).join('\n')
             html += 
                 `<button id='cancel' class='btn btn-danger'>Cancel</button>
-                <button id='submit' class='btn btn-success'>Submit</button>`
+                 <button id='submit' class='btn btn-success'>Submit</button>`
             ;
             parentElement.innerHTML = html;
             document.querySelector("#submit").onclick = () => {
-                for(const key in data) {
-                    result.push(document.getElementById(key).value);
-                    document.getElementById(key).value = "";
-                }
-                callback((result) => {
-                    console.log(result);
+                const result = data.map((name) => {
+                    return document.querySelector("#" + name).value;
                 });
+                callback(result);
             }
             document.querySelector("#cancel").onclick = () => {
                 for(const key in data) document.getElementById(key).value = "";
