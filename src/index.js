84df10c3378581a94d201a429f58d2954948d3d4
Angelo <ripamontiangelo@itis-molinari.eu>
2024-10-30 10:21:04 +0100

    test moment

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
-import { fetchCache } from "./scripts/fetchCache.js";
-
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const moment = require("moment");
+moment.locale("it");
+const monday = (moment().startOf('week'));        
+const mondayDate = new Date(monday.format());
+console.log(mondayDate.toLocaleDateString());
+const tue = new Date(new Date().setDate(mondayDate.getDate() + 1));
+console.log(tue.toLocaleDateString());
