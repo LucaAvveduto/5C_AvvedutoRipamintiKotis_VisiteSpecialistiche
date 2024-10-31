b7fd26070aa60e462147d0c4696c017a0ebbd8b9
Angelo <ripamontiangelo@itis-molinari.eu>
2024-10-31 12:39:07 +0100

    fetch as component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import moment from "../node_modules/moment/dist/moment.js";
+/*import moment from "../node_modules/moment/dist/moment.js";
 moment.locale("it");
 const monday = (moment().startOf('week'));        
 const mondayDate = new Date(monday.format());
 console.log(mondayDate.toLocaleDateString());
 const tue = new Date(new Date().setDate(mondayDate.getDate() + 1));
 console.log(tue.toLocaleDateString()); 
-/*
+*/
 import {navBarCreate} from "./scripts/navbar.js";
+import { generateFetchComponent } from "./scripts/fetchCache.js";
 
-navBarCreate();*/
+navBarCreate();
+const fetchComp = generateFetchComponent();
+fetchComp.build("../../config.json");
--- src/scripts/fetchCache.js
+++ src/scripts/fetchCache.js
@@ -1,24 +1,45 @@
 import { parseConfiguration } from "./jsonParser.js" 
-export function fetchCache(service, data) { 
-    return parseConfiguration("../../config.json").then(config => {
-        return new Promise(function (reso, reje) {
-            fetch(config.cacheURL + service, {
-                method: "POST",
-                body: data ? JSON.stringify({
-                    key: config.cacheKey,
-                    value: JSON.stringify(data)
-                }) : JSON.stringify({
-                    key: config.cacheKey,
-                }),
-                headers: {
-                    "Content-Type": "application/json",
-                    "key": config.cacheToken
-                }
+
+export function generateFetchComponent () {
+    let config;
+    return {
+        build : (pathConfig) => {
+            parseConfiguration("../../config.json").then(c => config = c).catch(console.error);
+        },        
+        setData: (data) => {
+            return new Promise((resolve, reject) => {
+                fetch(config.cacheURLSet, {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                        "key": config.cacheToken
+                    },
+                    body: JSON.stringify({
+                        key: config.cacheKey,
+                        value: JSON.stringify(data)
+                    })
+                })
+                .then(r => r.json())
+                .then(data => resolve(data.result))
+                .catch(err => reject(err.result));
+            });
+        },
+        getData: () => {
+            return new Promise((resolve, reject) => {
+                fetch(config.cacheURLGet, {
+                    method: "POST",
+                    headers: {
+                        "content-type": "application/json",
+                        "key": config.cacheToken
+                    },
+                    body: JSON.stringify({
+                        key: config.cacheKey
+                    })
+                })
+                .then(r => r.json())
+                .then(data => resolve(data.result))
+                .catch(err => reject(err.result));
             })
-                .then((reponse) => reponse.json())
-                .then((data) => reso(data))
-                .catch((error) => reje(error));
-        });
-    })
-    .catch(console.error);
+        }
+    };
 }
