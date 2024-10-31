b2903497b7d8224162f2cdc52e811f8bc3467b6e
Angelo <ripamontiangelo@itis-molinari.eu>
2024-10-31 12:20:59 +0100

    fix tailwindcss e import moment

--- src/index.js
+++ src/index.js
@@ -1,13 +1,11 @@
-/*import { createRequire } from "module";
-const require = createRequire(import.meta.url);
-const moment = require("moment");
+import moment from "../node_modules/moment/dist/moment.js";
 moment.locale("it");
 const monday = (moment().startOf('week'));        
 const mondayDate = new Date(monday.format());
 console.log(mondayDate.toLocaleDateString());
 const tue = new Date(new Date().setDate(mondayDate.getDate() + 1));
 console.log(tue.toLocaleDateString()); 
-*/
+/*
 import {navBarCreate} from "./scripts/navbar.js";
 
-navBarCreate();
+navBarCreate();*/
