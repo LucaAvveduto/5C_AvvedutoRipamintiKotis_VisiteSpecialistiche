56b9da61e2c789292228fd033107200fba5f15a4
AngeloRipamonti <ripamontiangelo@itis-molinari.eu>
2024-11-01 16:09:47 +0100

    fix index

--- src/index.js
+++ src/index.js
@@ -1,14 +1,28 @@
-/*import moment from "../node_modules/moment/dist/moment.js";
+import moment from "../node_modules/moment/dist/moment.js";
+import {navBarCreate} from "./scripts/navbar.js";
+import { createForm } from "./scripts/createForm.js";
+import { createTable } from "./scripts/createTable.js";
+import { generateFetchComponent } from "./scripts/fetchCache.js"
+
+
 moment.locale("it");
-const monday = (moment().startOf('week'));        
+const monday = (moment().startOf('week'));
+console.log(monday.format("dddd"))
 const mondayDate = new Date(monday.format());
 console.log(mondayDate.toLocaleDateString());
 const tue = new Date(new Date().setDate(mondayDate.getDate() + 1));
 console.log(tue.toLocaleDateString()); 
-*/
-import {navBarCreate} from "./scripts/navbar.js";
-import { generateFetchComponent } from "./scripts/fetchCache.js";
+
+const f = createForm(document.querySelector(".content"));
+f.setLabels(["Data","Ora","Nominativo"]);
+f.onsubmit(console.log)
+f.render();
 
 navBarCreate();
 const fetchComp = generateFetchComponent();
 fetchComp.build("../../config.json");
+
+const table = createTable(document.getElementById("cicco"));
+table.buildTable().then(() => {
+    table.render("Cardiologia");
+}).catch(console.error)
