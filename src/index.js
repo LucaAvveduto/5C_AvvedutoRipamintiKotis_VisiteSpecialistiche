61d07c0ac45933c25cbaf2ffccd994dac34ce189
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2024-11-02 14:17:50 +0100

    buttons and style

--- src/index.js
+++ src/index.js
@@ -1,21 +1,10 @@
-import moment from "../node_modules/moment/dist/moment.js";
 import {navBarCreate} from "./scripts/navbar.js";
 import { createForm } from "./scripts/createForm.js";
 import { createTable } from "./scripts/createTable.js";
 import { generateFetchComponent } from "./scripts/fetchCache.js"
 
-
-moment.locale("it");
-const monday = (moment().startOf('week'));
-console.log(monday.format("dddd"))
-const mondayDate = new Date(monday.format());
-console.log(mondayDate.toLocaleDateString());
-const tue = new Date(new Date().setDate(mondayDate.getDate() + 1));
-console.log(tue.toLocaleDateString()); 
-
 const f = createForm(document.querySelector(".content"));
 f.setLabels(["Data","Ora","Nominativo"]);
-f.onsubmit(console.log)
 f.render();
 
 navBarCreate();
