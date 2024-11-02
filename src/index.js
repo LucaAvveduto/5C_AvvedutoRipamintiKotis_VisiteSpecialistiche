a9be7a35265d91a95d27e5c14fc93e8e7d138b14
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2024-11-02 14:43:22 +0100

    table move

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-import {navBarCreate} from "./scripts/navbar.js";
+import { navBarCreate } from "./scripts/navbar.js";
 import { createForm } from "./scripts/createForm.js";
 import { createTable } from "./scripts/createTable.js";
 import { generateFetchComponent } from "./scripts/fetchCache.js"
+
+const forwardButton = document.getElementById("ahead");
+const backButton = document.getElementById("back");
+let offset = 0;
 
 const f = createForm(document.querySelector(".content"));
 f.setLabels(["Data","Ora","Nominativo"]);
@@ -15,3 +19,15 @@ const table = createTable(document.getElementById("avabTable"));
 table.buildTable().then(() => {
     table.render("Cardiologia");
 }).catch(console.error)
+
+forwardButton.onclick = () => {
+    offset++;
+    table.render("Cardiologia",offset);
+    console.log("Avanti");
+};
+
+backButton.onclick = () => {
+    offset--;
+    table.render("Cardiologia",offset);
+    console.log("Indietro");
+};
