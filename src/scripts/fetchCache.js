9b6744d47316d715046993eb8e40bf219f67b410
Luca Avveduto <avvedutoluca@itis-molinari.eu>
2024-11-03 11:32:02 +0100

    config usage

--- src/scripts/fetchCache.js
+++ src/scripts/fetchCache.js
@@ -33,7 +33,7 @@ export function generateFetchComponent() {
         },
         getData: () => {
             return new Promise((resolve, reject) => {
-                fetch("https://ws.cipiaceinfo.it/cache/get", {
+                fetch(config.cacheURLGet, {
                         method: "POST",
                         headers: {
                             "content-type": "application/json",
