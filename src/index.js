a4cf49d37f401288aab16ae47bf975a789c602bb
Alexandros Kotis <kotisalexandros@itis-molinari.eu>
2024-11-04 11:04:18 +0100

    complete index

--- src/index.js
+++ src/index.js
@@ -10,65 +10,64 @@ let offset = 0;
 
 
 const fetchComp = generateFetchComponent();
-fetchComp.build("../../config.json").then(console.log).catch(console.error);
+fetchComp.build("../../config.json").then(() => {
+    const table = createTable(document.getElementById("avabTable"));
+    table.buildTable().then(console.log).catch(console.error)
 
-const table = createTable(document.getElementById("avabTable"));
-table.buildTable().then(console.log).catch(console.error)
 
+    const navbar = navBarComponent(document.getElementById("navbar"));
+    navbar.callback((element) => {
+        forwardButton.onclick = () => {
+            offset++;
+            table.render(element, offset);
+        };
 
-const navbar = navBarComponent(document.getElementById("navbar"));
-navbar.callback((element) => {
-    forwardButton.onclick = () => {
-        offset++;
+        backButton.onclick = () => {
+            offset--;
+            table.render(element, offset);
+        };
+
         table.render(element, offset);
-    };
-
-    backButton.onclick = () => {
-        offset--;
-        table.render(element, offset);
-    };
-
-    table.render(element, offset);
-    const f = createForm(document.querySelector(".content"));
-    f.setLabels(["Data", "Ora", "Nominativo"]);
-    f.oncancel(() => { table.render(element, offset); });
-    f.onsubmit((values) => {
-        let validateInput;
-        const date = moment(values[0], "YYYY/MM/DD");
-        const closed = ["Saturday", "Sunday"];
-        if (date.calendar() < moment().calendar("MM/DD/YYYY") || closed.includes(date.format("dddd")) || isNaN(values[1])) validateInput = false;
-        const key = [element, date.format("DDMMYYYY"), values[1]].join("-");
-        console.log(validateInput);
-        fetchComp.getData().then((respose) => {
-            const json = JSON.parse(respose);
-            if (!json[key] && validateInput === undefined) {
-                json[key] = values[2];
-                fetchComp.setData(json).then(() => {
-                    table.render(element, offset);
-                    validateInput = true;
-                    document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
-                }).catch((error) => {
-                    console.log(error);
+        const f = createForm(document.querySelector(".content"));
+        f.setLabels(["Data", "Ora", "Nominativo"]);
+        f.oncancel(() => { table.render(element, offset); });
+        f.onsubmit((values) => {
+            let validateInput;
+            const date = moment(values[0], "YYYY/MM/DD");
+            const closed = ["Saturday", "Sunday"];
+            if (date.calendar() < moment().calendar("MM/DD/YYYY") || closed.includes(date.format("dddd")) || isNaN(values[1])) validateInput = false;
+            const key = [element, date.format("DDMMYYYY"), values[1]].join("-");
+            console.log(validateInput);
+            fetchComp.getData().then((respose) => {
+                const json = JSON.parse(respose);
+                if (!json[key] && validateInput === undefined) {
+                    json[key] = values[2];
+                    fetchComp.setData(json).then(() => {
+                        table.render(element, offset);
+                        validateInput = true;
+                        document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
+                    }).catch((error) => {
+                        console.log(error);
+                        validateInput = false;
+                        document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
+                    });
+                } else {
                     validateInput = false;
                     document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
-                });
-            } else {
+                }
+            }).catch((error) => {
+                console.log(error);
                 validateInput = false;
                 document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
-            }
-        }).catch((error) => {
-            console.log(error);
-            validateInput = false;
-            document.getElementById("result").innerHTML = validateInput === true ? "Ok" : "Ko";
+            });
         });
-    });
-    f.render();
-    setInterval(()=>{
-        table.render(element, offset);
-    }, 300000);
-})
+        f.render();
+        setInterval(() => {
+            table.render(element, offset);
+        }, 300000);
+    })
 
-navbar.build("../../config.json").then(() => {
-    navbar.render();
+    navbar.build("../../config.json").then(() => {
+        navbar.render();
+    }).catch(console.error);
 }).catch(console.error);
-
